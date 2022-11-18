a7be5972afcafd66436ac1dd270bfa2590d1b3cc
SageScroll18144 <ffss@cin.ufpe.br>
2022-11-18 00:44:05 -0300

    ampliando tela

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import pygame as pg
 
 BLUE = (0,0,255)
 
-screen = pg.display.set_mode((500,500))
+screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
 
 def drawCircle(screen, x, y):
     pg.draw.circle( screen, BLUE, ( x, y ), 5 )
@@ -11,7 +11,8 @@ isPressed = False
 
 while True:
     for event in pg.event.get():
-        if event.type == pg.QUIT:
+        keys = pg.key.get_pressed()
+        if event.type == pg.QUIT or keys[pg.K_q]:
             #se clicar em fechar, fecha o jogo
             pg.quit()
             sys.exit() 
