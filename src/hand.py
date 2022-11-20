123723c67a6d5c399be285b68ac92dc30554baa5
SageScroll18144 <ffss@cin.ufpe.br>
2022-11-20 10:27:29 -0300

    distancia euclidiana dos dedos implementada

--- src/hand.py
+++ src/hand.py
@@ -1,52 +1,20 @@
 import cv2
 import mediapipe as mp
+import math
+
+WINDOW_NAME = "bora billl"
+
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
-# For static images:
-IMAGE_FILES = []
-with mp_hands.Hands(
-    static_image_mode=True,
-    max_num_hands=2,
-    min_detection_confidence=0.5) as hands:
-  for idx, file in enumerate(IMAGE_FILES):
-    # Read an image, flip it around y-axis for correct handedness output (see
-    # above).
-    image = cv2.flip(cv2.imread(file), 1)
-    # Convert the BGR image to RGB before processing.
-    results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-    # Print handedness and draw hand landmarks on the image.
-    print('Handedness:', results.multi_handedness)
-    if not results.multi_hand_landmarks:
-      continue
-    image_height, image_width, _ = image.shape
-    annotated_image = image.copy()
-    for hand_landmarks in results.multi_hand_landmarks:
-      print('hand_landmarks:', hand_landmarks)
-      print(
-          f'Index finger tip coordinates: (',
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width}, '
-          f'{hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height})'
-      )
-      mp_drawing.draw_landmarks(
-          annotated_image,
-          hand_landmarks,
-          mp_hands.HAND_CONNECTIONS,
-          mp_drawing_styles.get_default_hand_landmarks_style(),
-          mp_drawing_styles.get_default_hand_connections_style())
-    cv2.imwrite(
-        '/tmp/annotated_image' + str(idx) + '.png', cv2.flip(annotated_image, 1))
-    # Draw hand world landmarks.
-    if not results.multi_hand_world_landmarks:
-      continue
-    for hand_world_landmarks in results.multi_hand_world_landmarks:
-      mp_drawing.plot_landmarks(
-        hand_world_landmarks, mp_hands.HAND_CONNECTIONS, azimuth=5)
+def euclian_distance(x,y,z):
+  return (math.sqrt(x**2+y**2+z**2)) <= 0.09
 
 # For webcam input:
 cap = cv2.VideoCapture(0)
+# cv2.namedWindow(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN)
+# cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 with mp_hands.Hands(
     model_complexity=0,
     min_detection_confidence=0.5,
@@ -75,6 +43,13 @@ with mp_hands.Hands(
             mp_hands.HAND_CONNECTIONS,
             mp_drawing_styles.get_default_hand_landmarks_style(),
             mp_drawing_styles.get_default_hand_connections_style())
+      #print([
+      if euclian_distance(results.multi_hand_landmarks[0].landmark[8].x - results.multi_hand_landmarks[0].landmark[12].x, results.multi_hand_landmarks[0].landmark[8].y - results.multi_hand_landmarks[0].landmark[12].y, results.multi_hand_landmarks[0].landmark[8].z - results.multi_hand_landmarks[0].landmark[12].z):
+        print("PINTANDO")
+      else: 
+        print("AGORA NAOO")
+        
+      print(results.multi_hand_landmarks[0].landmark[8])
     # Flip the image horizontally for a selfie-view display.
     cv2.imshow('MediaPipe Hands', cv2.flip(image, 1))
     if cv2.waitKey(5) & 0xFF == 27:
--- src/main.py
+++ src/main.py
@@ -1,21 +1,24 @@
 import pygame as pg
 
-BLUE = (0,0,255)
+BLACK = (0,0,0)
 
 screen = pg.display.set_mode((0,0), pg.FULLSCREEN)
+screen.fill((255,255,255))
 
 def drawCircle(screen, x, y):
-    pg.draw.circle( screen, BLUE, ( x, y ), 5 )
+    pg.draw.circle( screen, BLACK, ( x, y ), 5 )
 
 isPressed = False
+flag = True
 
-while True:
+while flag:
     for event in pg.event.get():
         keys = pg.key.get_pressed()
         if event.type == pg.QUIT or keys[pg.K_q]:
             #se clicar em fechar, fecha o jogo
             pg.quit()
-            sys.exit() 
+            flag = False
+            #sys.exit() 
         elif event.type == pg.MOUSEBUTTONDOWN:
             isPressed = True
         elif event.type == pg.MOUSEBUTTONUP:
@@ -23,4 +26,5 @@ while True:
         elif event.type == pg.MOUSEMOTION and isPressed == True:               
             ( x, y ) = pg.mouse.get_pos()
             drawCircle(screen, x, y)
-    pg.display.flip()
+    if flag:
+        pg.display.flip()
